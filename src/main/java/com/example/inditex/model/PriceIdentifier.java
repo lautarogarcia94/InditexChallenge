646f46f0778caba8a70ce80c42a232ab281b65fb
Lautaro Garcia <lautygcr@gmail.com>
2021-12-12 18:23:56 -0300

    Add price identifier model

---
+++ src/main/java/com/example/inditex/model/PriceIdentifier.java
@@ -0,0 +1,24 @@
+package com.example.inditex.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class PriceIdentifier {
+
+    @Min(value = 0, message = "Brand ID should be bigger than 0")
+    @NotNull(message = "Brand ID cannot be null")
+    private Integer brandId;
+
+    @NotBlank(message = "Application date cannot be null")
+    @Pattern(regexp = "[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}\\.[0-9]{2}\\.[0-9]{2}", message = "Application date is not in the correct format")
+    private String applicationDate;
+
+    @Min(value = 0, message = "Product ID should be bigger than 0")
+    @NotNull(message = "Product ID cannot be null")
+    private Integer productId;
+}
